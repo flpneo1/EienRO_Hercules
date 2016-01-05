f382199c3e2962434b114e155d3c7e7d67f8162d
flpneo1 <fer_lp_4@hotmail.com>
2016-01-05 10:37:16 -0200

    Update 05-01-16
    Atualizações de npcs, itemdb e criado atalhos para ligar somente login
    server.

---
+++ run-server - sem login.bat
@@ -0,0 +1,7 @@
+@echo off
+rem This is and auto-restart script for the Server Emulator.
+rem It will also keep the map server OPEN after it crashes to that errors may be
+rem more easily identified
+rem Writen by Jbain
+start cmd /k char-server.bat
+start cmd /k map-server.bat
